c88a71671caf2c5393b61aaf5b15625694cae107
HyunsooZo <bzhs1992@icloud.com>
2024-09-09 20:35:18 +0900

    chore (build) : 프로젝트 세팅
    - Gradle wrapper 설정 추가
    - 기본 의존성 추가 (Spring WebFlux, MongoDB 등)

---
+++ src/main/kotlin/com/immilog/chatserver/ChatserverApplication.kt
@@ -0,0 +1,11 @@
+package com.immilog.chatserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/immilog/chatserver/ChatserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.immilog.chatserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
